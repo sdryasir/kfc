380fc2fbbcb27950a3cdbda60822798c15890cfe
sdryasir <sdr.yasir@gmail.com>
2022-05-29 02:34:28 -0700

    Backend setup and product CRUD done

---
+++ backend/routes/products.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {createProduct, getAllProducts, getProductById, updateProduct, deleteProduct} = require('../controllers/productController')
+
+router.route('/product/new').post(createProduct);
+router.route('/products').get(getAllProducts);
+router.route('/product/:id').get(getProductById);
+router.route('/product/update/:id').put(updateProduct);
+router.route('/product/delete/:id').delete(deleteProduct);
+
+module.exports = router
+
+
+
+
+
